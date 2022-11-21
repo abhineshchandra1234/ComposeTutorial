d886c7c586e0340f352f18e153f35cf8aadd2abb
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2022-11-21 22:00:15 +0530

    covered layouts

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,34 +1,58 @@
 package com.example.composetutorial
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "Abhinesh")
+            MessageCard(Message("android", "jetpack compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all=8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(name = "Abhinesh")
+    MessageCard(msg = Message("android", "jetpack compose"))
 }
 
